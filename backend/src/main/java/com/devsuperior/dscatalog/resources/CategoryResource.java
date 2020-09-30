daeba536b2591286ab7171cd4f895e1c6a4dfd6b
Keran Barbieri <keranbarbieri@hotmail.com>
2020-09-30 18:03:46 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.entities.Category;
-import com.devsuperior.dscatalog.rservices.CategoryService;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 /*
  * Classe que implementa o controlador REST
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,9 +1,10 @@
-package com.devsuperior.dscatalog.rservices;
+package com.devsuperior.dscatalog.services;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -22,6 +23,10 @@ public class CategoryService {
 	@Autowired
     private CategoryRepository repository;
     
+	/*
+	 * Transactional garante a integridade da transação
+	 */
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
 		
