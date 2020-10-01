e82b96c3440304461d806eb3b5c93b5388586460
Keran Barbieri <keranbarbieri@hotmail.com>
2020-10-01 20:15:03 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 
 /*
  * Classe que implementa o controlador REST
@@ -30,4 +32,10 @@ public class CategoryResource {
 		
 	}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity <CategoryDTO> findById(@PathVariable Long id) throws EntityNotFoundException{
+	CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+		
+	}
 }
